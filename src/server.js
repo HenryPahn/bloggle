8bede2a9e24c1f208146a8da3bc9b56c9a16692c
HenryPahn <thoang0303@gmail.com>
2025-02-15 13:44:16 -0500

    Add env info to logger when run dev and debug script

--- src/server.js
+++ src/server.js
@@ -9,6 +9,16 @@ const port = parseInt(process.env.PORT || '8080', 10);
 // Start a server listening on this port
 const server = stoppable(
   app.listen(port, () => {
+    // Check if the current npm script is 'dev' or 'debug'
+    const isDebugging = process.env.npm_lifecycle_event === 'dev' || process.env.npm_lifecycle_event === 'debug';
+
+    // if user is debugging, print all environment variables to check if any is missing	  
+    if (isDebugging) {
+      logger.debug(`Environment Variables: 
+- LOG_LEVEL: ${process.env.LOG_LEVEL}
+- PORT: ${process.env.PORT}`);
+    }
+
     // Log a message that the server has started, and which port it's using.
     logger.info(`Server is running on port ${port}`);
   })
