365e89eec2e650a315a93ef30243b88c0ac69db2
HenryPahn <thoang0303@gmail.com>
2025-02-17 11:42:22 -0500

    Add response

--- src/routes/api/get.js
+++ src/routes/api/get.js
@@ -3,11 +3,20 @@
 const express = require('express');
 const { db } = require('../../firebase');
 
+const { createSuccessResponse } = require('../response')
+
 module.exports = async (req, res) => {
   try {
     const snapshot = await db.collection('tests').get();
     const data = snapshot.docs.map(doc => doc.data());
-    res.status(200).json(data);
+
+    // create a success response
+    const successResponse = createSuccessResponse({
+      ...data
+    })
+
+    
+    res.status(200).json(successResponse);
   } catch (error) {
     console.error('Error fetching data:', error);
     res.status(500).send('Internal Server Error');
