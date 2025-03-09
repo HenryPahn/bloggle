6a253ff884e86ce2093f7d9f21f0d6f7174ab444
HenryPahn <thoang0303@gmail.com>
2025-03-09 15:20:00 -0400

    Add Jest and tests

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,14 @@ import pluginJs from "@eslint/js";
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.js"], languageOptions: {sourceType: "commonjs"}},
-  {languageOptions: { globals: globals.node }},
+  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
+  {
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.jest,
+      },
+    },
+  },
   pluginJs.configs.recommended,
 ];
