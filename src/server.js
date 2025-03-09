712da19b2b957f7fdc42cc59819f861d38f0933e
HenryPahn <thoang0303@gmail.com>
2025-03-09 15:00:02 -0400

    Restructure API server

--- src/server.js
+++ src/server.js
@@ -1,22 +1,26 @@
 // src/server.js
-
-// We want to gracefully shutdown our server
 const stoppable = require('stoppable');
-
-// Get our logger instance
 const logger = require('./logger');
-
-// Get our express app instance
 const app = require('./app');
 
-// Get the desired port from the process' environment. Default to `8080`
+// Get PORT from .env file and default port is `8080`
 const port = parseInt(process.env.PORT || '8080', 10);
 
 // Start a server listening on this port
 const server = stoppable(
   app.listen(port, () => {
+    // Check if the current npm script is 'dev' or 'debug'
+    const isDebugging = process.env.npm_lifecycle_event === 'dev' || process.env.npm_lifecycle_event === 'debug';
+
+    // if user is debugging, print all environment variables to check if any is missing	  
+    if (isDebugging) {
+      logger.debug(`Environment Variables: 
+- LOG_LEVEL: ${process.env.LOG_LEVEL}
+- PORT: ${process.env.PORT}`);
+    }
+
     // Log a message that the server has started, and which port it's using.
-    logger.info(`Server started on port ${port}`);
+    logger.info(`Server is running on port ${port}`);
   })
 );
 
