0a7acc12abea932d9faf3c040a48cfaab755da67
Arlene <hoanglampham248@gmail.com>
2025-03-10 20:59:43 -0400

    Fixing error

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 // src/app.js
 
+
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
--- src/auth/index.js
+++ src/auth/index.js
@@ -1,3 +1,6 @@
 if (process.env.HTPASSWD_FILE && process.NODE_ENV !== 'production') {
   module.exports = require('./basic-auth');
 }
+else {
+  throw new Error('missing env vars: no authorization configuration found');
+}
