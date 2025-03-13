f473c2848fbdcd107dc37006956c314a1466fc47
HenryPahn <thoang0303@gmail.com>
2025-03-13 14:00:33 -0400

    Fix rebase

---
+++ src/model/firestore-db.js
@@ -0,0 +1,27 @@
+// src/model/data/firestore-db.js
+
+require("dotenv").config(); // Load environment variables
+const { getFirestore } = require("firebase/firestore");
+const { getStorage } = require("firebase/storage");
+const { getAuth } = require("firebase/auth");
+const { initializeApp } = require("firebase/app");
+
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.FIREBASE_DATABASE_URL,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const fireDB = getFirestore(app);
+const auth = getAuth(app);
+const storage = getStorage(app);
+
+module.exports.fireDB = fireDB;
+module.exports.auth = auth;
+module.exports.storage = storage;
