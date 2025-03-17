71432eed3bdd3ce02d639ac2b82bd9cd2db16b46
HenryPahn <thoang0303@gmail.com>
2025-03-16 23:15:42 -0400

    Fix CI error - Unknown firebase secrets

--- src/model/firestore-db.js
+++ src/model/firestore-db.js
@@ -1,29 +1,19 @@
-// src/model/firestore-db.js
+require("dotenv").config();
 
-// const admin = require("firebase-admin");
-// var serviceAccount = require("../../firebase/permission.json");
+const admin = require("firebase-admin");
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
 // admin.initializeApp({
 //   credential: admin.credential.cert(serviceAccount),
-//   storageBucket: "bloggle-9f7b4.firebasestorage.app",
+//   databaseURL: "https://bloggle-9f7b4-default-rtdb.firebaseio.com",
+//   storageBucket: "gs://bloggle-9f7b4.firebasestorage.app", 
 // });
-
-// const fireDB = admin.firestore();
-// const auth = admin.auth();
-// const storage = admin.storage().bucket("bloggle-9f7b4.firebasestorage.app"); 
-
-// module.exports.fireDB = fireDB;
-// module.exports.auth = auth;
-// module.exports.storage = storage;
-
-
-const admin = require("firebase-admin");
-const serviceAccount = require("../../firebase/permission.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
-  databaseURL: "https://bloggle-9f7b4-default-rtdb.firebaseio.com",
-  storageBucket: "gs://bloggle-9f7b4.firebasestorage.app", 
+  databaseURL: process.env.DATABASE_URL,
+  storageBucket: process.env.STORAGE_BUCKET, 
 });
 
 const fireDB = admin.firestore();
@@ -32,3 +22,4 @@ const storage = admin.storage();
 
 
 module.exports = { fireDB, auth, storage };
+
