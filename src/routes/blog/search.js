0d4854401f3ef3450517ce944efcdbc4ec034159
Nazanin Tabrizizadeh <naz.tbz7@gmail.com>
2025-03-17 16:28:16 -0400

    updated for search.js

--- src/routes/blog/search.js
+++ src/routes/blog/search.js
@@ -1,31 +1,32 @@
-// src/routes/blog/search.js
-
-// Functions return successful responses 
-const {Blog} = require('../../model/blog');
-const { createSuccessResponse } = require('../../response');
+const { Blog } = require('../../model/blog');
+const { createSuccessResponse, createErrorResponse } = require('../../response');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
   // Extracting query parameters from the request (search keyword and category)
-  const {keyword, category} = req.query;
+  const { keyword, category } = req.query;
 
-  try{
+  try {
     // Checking if the keyword is not provided
-    if(!keyword){ 
-      return res.status(200).json(createSuccessResponse({message:'Keyword must be provided'}));
+    if (!keyword) { 
+      logger.error({ errMessage: 'Keyword must be provided' }, 'GET /blog/search - Bad Request');
+      return res.status(400).json(createErrorResponse(400, 'Keyword must be provided'));
     }
+
     // Searching for blogs based on the provided keyword and category
-    const blogs = await Blog.search(keyword,category);
+    const blogs = await Blog.search(keyword, category);
 
-    if(blogs.length > 0){ // If blogs are found
-      blogs.sort((a,b) => new Date(b.created) - new Date(a.created)); // Sorting blogs by their creation date in descending order
-      res.status(200).json(createSuccessResponse({blogs:blogs}));
+    if (blogs.length > 0) { // If blogs are found
+      blogs.sort((a, b) => new Date(b.created) - new Date(a.created)); // Sorting blogs by creation date (latest first)
+      
+      logger.info({ blogsCount: blogs.length }, 'GET /blog/search - Blogs retrieved successfully');
+      return res.status(200).json(createSuccessResponse({ blogs }));
     } else {
-      return res.status(200).json(createSuccessResponse({message:'No matching blogs found'}));
+      logger.info({}, 'GET /blog/search - No matching blogs found');
+      return res.status(200).json(createSuccessResponse({ message: 'No matching blogs found' }));
     }
-    } catch (error) {
-      logger.error('Error occured in search API:', error);
-      return res.status(200).json(createSuccessResponse({message:'An error occured while searching blogs'}));
-    }
-  };
-
+  } catch (error) {
+    logger.error({ errMessage: error.message }, 'GET /blog/search - Error searching blog');
+    return res.status(500).json(createErrorResponse(500, 'Interval Server Error'));
+  }
+};
