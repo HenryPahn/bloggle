e15e453bee04a0b94d79abf7b975716c3bad456b
HenryPahn <thoang0303@gmail.com>
2025-03-16 22:29:28 -0400

    Change firebase config

--- src/model/firestore-db.js
+++ src/model/firestore-db.js
@@ -1,40 +1,34 @@
-// // src/model/data/firestore-db.js
+// src/model/firestore-db.js
 
-// require("dotenv").config(); // Load environment variables
-// const { getFirestore } = require("firebase/firestore");
-// const { getStorage } = require("firebase/storage");
-// const { getAuth } = require("firebase/auth");
-// const { initializeApp } = require("firebase/app");
+// const admin = require("firebase-admin");
+// var serviceAccount = require("../../firebase/permission.json");
 
-// const firebaseConfig = {
-//   apiKey: process.env.FIREBASE_API_KEY,
-//   authDomain: process.env.FIREBASE_AUTH_DOMAIN,
-//   databaseURL: process.env.FIREBASE_DATABASE_URL,
-//   projectId: process.env.FIREBASE_PROJECT_ID,
-//   storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
-//   messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
-//   appId: process.env.FIREBASE_APP_ID
-// };
+// admin.initializeApp({
+//   credential: admin.credential.cert(serviceAccount),
+//   storageBucket: "bloggle-9f7b4.firebasestorage.app",
+// });
 
-// // Initialize Firebase
-// const app = initializeApp(firebaseConfig);
-// const fireDB = getFirestore(app);
-// const auth = getAuth(app);
-// const storage = getStorage(app);
+// const fireDB = admin.firestore();
+// const auth = admin.auth();
+// const storage = admin.storage().bucket("bloggle-9f7b4.firebasestorage.app"); 
 
 // module.exports.fireDB = fireDB;
 // module.exports.auth = auth;
 // module.exports.storage = storage;
-const admin = require("firebase-admin");
 
-if (!admin.apps.length) {
-  admin.initializeApp({
-    credential: admin.credential.applicationDefault()
-  });
-}
+
+const admin = require("firebase-admin");
+const serviceAccount = require("../../firebase/permission.json");
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://bloggle-9f7b4-default-rtdb.firebaseio.com",
+  storageBucket: "gs://bloggle-9f7b4.firebasestorage.app", 
+});
 
 const fireDB = admin.firestore();
 const auth = admin.auth();
-const storage = admin.storage();
+const storage = admin.storage(); 
+
 
 module.exports = { fireDB, auth, storage };
