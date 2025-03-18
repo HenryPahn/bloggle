4a0b5642a5bd8141a963e08bc625484d62a75d3c
Nazanin Tabrizizadeh <naz.tbz7@gmail.com>
2025-03-18 12:53:14 -0400

    updated index.js for visitedBlogs

--- src/routes/blogger/index.js
+++ src/routes/blogger/index.js
@@ -10,10 +10,20 @@ router.get(`/favorite`, require('./favoriteBlogs/get'));
 
 // Other routes (POST, DELETE, etc.) for favourite blogs will go here later on...
 
+
+
+// Other routes (POST, DELETE, etc.) for visited blogs will go here later on...
 //  GET /blogger/visited
 router.get(`/visited`, require('./visitedBlogs/get')); 
 
-// Other routes (POST, DELETE, etc.) for visited blogs will go here later on...
+// POST /blogger/visited/:blogId 
+router.post('/visited/:blogId', require('./visitedBlogs/post'));
+
+//DELETE /blogger/visited/:blogId
+router.delete('/visited/:blogId', require('./visitedBlogs/delete'));
+
+
+
 
 module.exports = router;
 
