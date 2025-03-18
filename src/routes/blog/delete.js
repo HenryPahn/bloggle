0b999986a2e836ac84c7ec006c7d11a8081dffd9
zlinzz <z332119551@gmail.com>
2025-03-18 18:45:26 -0400

    Fix DELETE /blog/:id - Ensure 404 response for non-existing blog posts

--- src/routes/blog/delete.js
+++ src/routes/blog/delete.js
@@ -4,25 +4,40 @@ const { createSuccessResponse, createErrorResponse } = require('../../response')
 const { Blog } = require('../../model/blog');
 const logger = require('../../logger');
 
+/**
+ * Delete the user's own blog post with blog id
+ */
 module.exports = async (req, res) => {
+  const ownerId = req.user;
+  const { id } = req.params;
+
   try {
-    const ownerId = req.user;
-    const { id } = req.params;
-
     logger.info(`DELETE /blog/:id - Incoming request to delete a blog post`);
-    const deleted = await Blog.delete(ownerId, id);
 
-    if (deleted) {
-      logger.info({ ownerId, id }, 'DELETE /blog/:id - Deleted blog post successfully.');
-      return res
-        .status(200)
-        .json(createSuccessResponse({ message: 'Blog post deleted successfully' }));
+    await Blog.delete(ownerId, id);
+
+    logger.info({ ownerId, removeBid: id }, 'DELETE /blog/:id - Deleted blog post successfully.');
+    return res
+      .status(200)
+      .json(createSuccessResponse({ message: 'Blog post deleted successfully' }));
+  } catch (err) {
+    if (err.message.includes('Owner ID and Blog ID are required')) {
+      logger.warn(
+        { ownerId, removeBid: id, errMessage: err.message },
+        `DELETE /blog/:id - Missing required fields`
+      );
+      return res.status(400).json(createErrorResponse(400, err.message));
     }
 
-    logger.warn({ ownerId, id }, 'DELETE /blog/:id - Blog post not found');
-    return res.status(404).json(createErrorResponse(404, 'Fail to delete: Blog post not found'));
-  } catch (err) {
+    if (err.message.includes('No blog found for owner')) {
+      logger.warn(
+        { ownerId, removeBid: id, errMessage: err.message },
+        `DELETE /blog/:id - Blog not found`
+      );
+      return res.status(404).json(createErrorResponse(404, err.message));
+    }
+
     logger.error({ errMessage: err.message }, 'DELETE /blog/:id - Internal Server Error');
-    return res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
+    return res.status(500).json(createErrorResponse(500, `Internal Server Error: ${err.message}`));
   }
 };
