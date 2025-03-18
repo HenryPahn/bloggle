2d351b3b339e75920bbf1658814bf8dcfe67d3f7
zlinzz <z332119551@gmail.com>
2025-03-18 16:49:03 -0400

    Update route authentication requirements

--- src/routes/blog/index.js
+++ src/routes/blog/index.js
@@ -5,21 +5,24 @@ const express = require('express');
 // Create a router on which to mount our API endpoints
 const router = express.Router();
 
+// authentication middleware
+const { authenticate } = require('../../auth');
+
 /* CRUD Routes */
 // POST /blog -> Create a new blog post
-router.post(`/`, require('./post'));
+router.post(`/`, authenticate(), require('./post'));
 
 // PUT /blog/:id -> Update the user's own existing blog post with blog id
-router.put(`/:id`, require('./put'));
+router.put(`/:id`, authenticate(), require('./put'));
 
 // GET /blog -> Retrieve all blog posts for the current user
-router.get(`/`, require('./get'));
+router.get(`/`, authenticate(), require('./get'));
 
 // GET /blog/:id -> Retrieve a single blog post with id, returns blog post details
-router.get(`/:id`, require('./getById'));
+router.get(`/:id`, authenticate(), require('./getById'));
 
 // DELETE /blog/:id -> Delete the user's own blog post with blog id
-router.delete(`/:id`, require('./delete'));
+router.delete(`/:id`, authenticate(), require('./delete'));
 
 // -----------------------------------------
 /* Search Functionality Routes */
