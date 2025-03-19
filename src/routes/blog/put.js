5fba54740bcac5969a815c0bb2698d1e2f886da0
zlinzz <z332119551@gmail.com>
2025-03-19 01:06:32 -0400

    Update error handling

--- src/routes/blog/put.js
+++ src/routes/blog/put.js
@@ -5,13 +5,12 @@ const { Blog } = require('../../model/blog');
 const logger = require('../../logger');
 
 module.exports = async (req, res) => {
+  const ownerId = req.user;
+  const { id } = req.params;
+  const { title, content, images } = req.body;
+
   try {
     logger.info(`PUT /blog/:id - Incoming request to update a blog post`);
-
-    const ownerId = req.user;
-    const { id } = req.params;
-    // Extracting update info from req.body
-    const { title, content, images } = req.body;
 
     const blog = await Blog.byId(ownerId, id);
 
@@ -27,19 +26,26 @@ module.exports = async (req, res) => {
     // Save changes
     await blog.save();
 
-    logger.info({ blog: blog.getData() }, 'PUT /blog/:id - Blog updated successfully');
+    logger.debug({ blog: blog.getData() }, 'PUT /blog/:id - Blog updated successfully');
     return res.status(200).json(createSuccessResponse({ blog: blog.getData() }));
   } catch (err) {
-    if (err.message.includes(`The data isn't changed`)) {
-      logger.warn(
-        { message: err.message },
-        `PUT /blog/:id - The data isn't changed, there is no input`
-      );
-      return res
-        .status(400)
-        .json(createErrorResponse(400, `The data isn't changed, there is no input`));
+    if (
+      err.message.includes('Owner ID and Blog ID are required') ||
+      err.message.includes(`The data isn't changed`)
+    ) {
+      logger.warn({ ownerId, id, errMessage: err.message }, `PUT /blog/:id - Bad Request`);
+      return res.status(400).json(createErrorResponse(400, err.message));
     }
-    logger.error({ message: err.message }, 'PUT /blog/:id - Error updating blog post');
-    return res.status(500).json(createErrorResponse(500, 'Internal Server Error'));
+
+    if (err.message.includes('No blog found with')) {
+      logger.warn(
+        { ownerId, id, errMessage: err.message },
+        `PUT /blog/:id - Blog post not found for the user`
+      );
+      return res.status(404).json(createErrorResponse(404, err.message));
+    }
+
+    logger.error({ message: err.message }, 'PUT /blog/:id - Internal Server Error');
+    return res.status(500).json(createErrorResponse(500, `Internal Server Error: ${err.message}`));
   }
 };
